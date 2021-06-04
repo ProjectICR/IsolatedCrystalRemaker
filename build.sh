19e306fd3145761917980bd4246cefacc4f30b23
superhelo <3181063382@qq.com>
2021-06-04 19:09:49 +0800

    更新 build.sh

--- build.sh
+++ build.sh
@@ -2,21 +2,10 @@
 
 #!/bin/sh -x
 
-
-BUILD=IsolatedCrystalRemaker
-DATE="$(TZ='Asia/Shanghai' date +%Y-%m-%d_%H-%M)"
-#VERSION=1.0
-#VERSION=$"(echo r$(git rev-list --count HEAD).$(git rev-parse --short HEAD)"
-VERSION=DevEdition #被硬编码了
-COMMON_NAME="${BUILD}-${VERSION}-${DATE}"
-#JAR_FILE_NAME="${COMMON_NAME}.jar"
-ZIP_FILE_NAME="${COMMON_NAME}.zip"
-#PROJECT_DIR="${HOME}/Projects/example"
-
 [ -d "${GITHUB_WORKSPACE}" ] && PROJECT_DIR="${GITHUB_WORKSPACE}"
 [ "${PROJECT_DIR}" != "${PWD}" ] && cd "${PROJECT_DIR}"
 
-
-mv .minecraft overrides
-zip -r "${ZIP_FILE_NAME}" -xi manifest.json modlist.html overrides
-mv {,artifacts/}${ZIP_FILE_NAME}
+mkdir artifacts
+mv .minecraft artifacts/overrides
+mv manifest.json artifacts
+mv modlist.html artifacts
